8993ecf160d57fa9ccc88ce3003bb7754a0bc3d2
TroyAWilson <troyaustynwilson@gmail.com>
2019-08-10 23:44:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TroyaWilsonv2
---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+test = document.getElementsByClassName('big-name-1');
+
+
+$('.big-name-1').fadeTo(1000 ,1);
+$('.big-name-2').delay(2000).fadeTo(6000 ,1);
+$('.big-name-3').delay(2000).fadeTo(7000 ,1);
+$('.big-name-4').delay(2000).fadeTo(8000 ,1);
+$('.big-name-5').delay(2000).fadeTo(9000 ,1);
+$('.big-name-6').delay(4500).fadeTo(10000 ,1);
+$('.big-name-7').delay(4500).fadeTo(11000 ,1);
+$('.big-name-8').delay(4500).fadeTo(12000 ,1);
+$('.big-name-9').delay(4500).fadeTo(13000 ,1);
+$('.big-name-10').delay(4500).fadeTo(14000 ,1);
+$('.big-name-11').delay(4500).fadeTo(15000 ,1);
+
+$('.menu-1').delay(6000).fadeTo(3600,1);
