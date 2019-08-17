9c052ce7b05077f6cca7c5991b1844f620f6f9ee
TroyAWilson <troyaustynwilson@gmail.com>
2019-08-16 18:34:41 -0700

    New and updated pages

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,9 @@ $('.big-name-1').fadeTo(1000 ,1);
 $('.big-name-2').delay(2000).fadeTo(6000 ,1);
 $('.big-name-3').delay(2000).fadeTo(7000 ,1);
 $('.big-name-4').delay(2000).fadeTo(8000 ,1);
+$('.big-name-g').delay(2000).fadeTo(8000 ,1);
 $('.big-name-5').delay(2000).fadeTo(9000 ,1);
+$('.big-name-k').delay(2000).fadeTo(10000 ,1);
 $('.big-name-6').delay(4500).fadeTo(10000 ,1);
 $('.big-name-7').delay(4500).fadeTo(11000 ,1);
 $('.big-name-8').delay(4500).fadeTo(12000 ,1);
@@ -14,3 +16,6 @@ $('.big-name-10').delay(4500).fadeTo(14000 ,1);
 $('.big-name-11').delay(4500).fadeTo(15000 ,1);
 
 $('.menu-1').delay(6000).fadeTo(3600,1);
+$('.about-text').delay(6000).fadeTo(3600,1);
+$('.work-box').delay(6000).fadeTo(3600,1);
+$('.skills').delay(6000).fadeTo(3600,1);
